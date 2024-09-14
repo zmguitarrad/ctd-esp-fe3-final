6ad0fd85213e853794fcda318aee99ab789127e4
zmguitarrad <zguitarrad@gmail.com>
2024-09-14 09:11:37 -0500

    Add feature

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
     <footer>
       <div className="footer-container">
         <div className="logo">
-          <p>Powered by</p>
+          <p >Powered by</p>
           <img src="/images/DH.png" alt="DH-logo" />
         </div>
-
+     
         <div className="logos">
           <img src="/images/ico-facebook.png" alt="facebook logo" />
           <img src="/images/ico-instagram.png" alt="instagram logo" />
           <img src="/images/ico-whatsapp.png" alt="whatsapp logo" />
           <img src="/images/ico-tiktok.png" alt="tiktok logo" />
         </div>
-      </div>
+        </div>
     </footer>
   );
 };
